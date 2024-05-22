13595f15fccf40ffb2e60969f63ac1028ae2040c
Giscard Stephanou <giscard.silva@aluno.senai.br>
2024-05-21 21:06:22 -0300

    Versão 2.0 do Contador

--- contador.py
+++ contador.py
@@ -1,5 +1,16 @@
-contador = int(input('Informe um numero inteiro: '))
+#i importa as Bibliotecas 
+import os
+import time
 
-while contador >= 0:
-    print(contador)
+contador = int(input('Informe um numero inteiro: '))
+os.system('cls')
+
+while contador > 0:
+
+    print(f'Contagem regressiva: {contador}...')
+    time.sleep(1)
+    os.system('cls')
     contador -=1
+
+
+print('Boom!!!') 
